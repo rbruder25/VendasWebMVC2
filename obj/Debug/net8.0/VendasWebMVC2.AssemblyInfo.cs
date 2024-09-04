95d514078e9130148c6490a91615cea38f8b8f8e
Roberto Bruder <roberto_bruder@yahoo.com.br>
2024-09-04 13:06:09 -0300

    Seviço de Vendedores FindAll basico

--- obj/Debug/net8.0/VendasWebMVC2.AssemblyInfo.cs
+++ obj/Debug/net8.0/VendasWebMVC2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VendasWebMVC2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd718baefc7efbf9db9350f3bae7883ff9b2a1d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43caf9fd4a75a58bf97f02d4ac3acb97d1460b78")]
 [assembly: System.Reflection.AssemblyProductAttribute("VendasWebMVC2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VendasWebMVC2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
